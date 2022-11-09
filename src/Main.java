3aa0e03b8e9bb78ee79200203e3e4b1a653f2db5
jxosedev <victttor.dev@gmail.com>
2022-11-09 16:21:28 -0300

    "Criando funções na Main"

--- src/Main.java
+++ src/Main.java
@@ -19,23 +19,25 @@ public class Main {
         System.out.println("Escolha um tipo de conta: ");
         System.out.println("1- Conta Corrente");
         System.out.println("2- Conta Poupança");
+        System.out.println("3- Digite 0 para sair.");
 
         System.out.println("============================");
 
         int tipodeconta = entrada.nextInt();
 
-        while (tipodeconta == 1) {
+        if (tipodeconta == 1) {
             System.out.println("============================");
 
-            System.out.println("1- Ver extrato");
-            System.out.println("2- Fazer deposito");
-            System.out.println("3- Fazer transferencia");
+            System.out.println("1- Ver extrato.");
+            System.out.println("2- Fazer deposito.");
+            System.out.println("3- Fazer transferencia.");
+            System.out.println("4- Digite 0 para sair.");
 
             System.out.println("============================");
 
             int escolhacorrente = entrada.nextInt();
 
-            switch (escolhacorrente){
+            switch (escolhacorrente) {
                 case 1:
                     if (escolhacorrente == 1) {
                         Conta cc = new ContaCorrente();
@@ -56,8 +58,52 @@ public class Main {
                         cc.imprimirExtrto();
                         cp.imprimirExtrto();
                     }
+                case 4:
+                    if (escolhacorrente > 3) {
+                        System.out.println("Escolha uma opção válida !!!");
+                    }
             }
-            break;
+        }
+        if (tipodeconta == 2) {
+            System.out.println("============================");
+
+            System.out.println("1- Ver extrato.");
+            System.out.println("2- Fazer deposito.");
+            System.out.println("3- Fazer transferencia.");
+            System.out.println("4- Digite 0 para sair.");
+
+            System.out.println("============================");
+
+            int escolhacorrente = entrada.nextInt();
+
+            switch (escolhacorrente) {
+                case 1:
+                    if (escolhacorrente == 1) {
+                        Conta cc = new ContaCorrente();
+                        cc.imprimirExtrto();
+                    }
+                case 2:
+                    if (escolhacorrente == 2) {
+                        Conta cc = new ContaCorrente();
+                        cc.depositar(100);
+                        cc.imprimirExtrto();
+                    }
+                case 3:
+                    if (escolhacorrente == 3) {
+                        Conta cc = new ContaCorrente();
+                        Conta cp = new ContaPoupanca();
+                        cp.depositar(100);
+                        //cc.transferir(100,cp);
+                        cp.imprimirExtrto();
+                        cc.imprimirExtrto();
+                    }
+                case 4:
+                    if (escolhacorrente > 3) {
+                        System.out.println("Escolha uma opção válida !!!");
+                    }
+            }
+        } else {
+            System.out.println("Encerrando !!!");
         }
     }
 }
